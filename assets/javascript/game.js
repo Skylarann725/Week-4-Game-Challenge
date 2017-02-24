b649a2801da573a65228506ef376c6ff68908a57
Skylar Siagel <skylarsiagel@Skylars-MacBook-Pro.local>
2017-02-24 13:47:02 -0500

    added files

---
+++ assets/javascript/game.js
@@ -0,0 +1,69 @@
+//Character Objects
+var feyre = {
+    name: "Feyre",
+    id: "feyre",
+    attack: 0,
+    // img: "images/_.png",
+    hp: 0,
+    player: false,
+    opp: false,
+    defeated: false
+};
+
+var rhysand = {
+    name: "Rhysand",
+    id: "rhysand",
+    attack: 0,
+    hp: 0,
+    // img: "images/_.png",
+    player: false,
+    opp: false,
+    defeated: false
+};
+
+var tamlin = {
+    name: "Tamlin",
+    id: "tamlin",
+    attack: 0,
+    hp: 0,
+    // img: "images/_.png",
+    player: false,
+    opp: false,
+    defeated: false
+};
+
+var amarantha = {
+    name: "Amarantha",
+    id: "amarantha",
+    attack: 0,
+    hp: 0,
+    // img: "images/_.png",
+    player: false,
+    opp: false,
+    defeated: false
+};
+
+characterArray = [feyre, rhysand, tamlin, amarantha];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
